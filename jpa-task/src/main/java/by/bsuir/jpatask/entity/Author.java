d24c76a2eab700fe83f848936de9d59cbe6d1dcd
Dzmitry Haron <pitbull9846@gmail.com>
2024-10-06 15:59:27 +0300

    Add custom equals and hashCode methods for entities

--- jpa-task/src/main/java/by/bsuir/jpatask/entity/Author.java
+++ jpa-task/src/main/java/by/bsuir/jpatask/entity/Author.java
@@ -1,17 +1,19 @@
 package by.bsuir.jpatask.entity;
 
+import org.hibernate.proxy.HibernateProxy;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-@Data
+@Getter
+@Setter
 @SuperBuilder
 @jakarta.persistence.Entity
 @Table(name = "tbl_author")
@@ -28,5 +30,37 @@ public class Author extends Entity {
 
     @Column(length = 64, nullable = false)
     private String lastname;
+
+    @Override
+    public final boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null) {
+            return false;
+        }
+
+        Class<?> oEffectiveClass = object instanceof HibernateProxy ? ((HibernateProxy) object).getHibernateLazyInitializer()
+                                                                                               .getPersistentClass() 
+                                                                    : object.getClass();
+                                                                    
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                                                                                              .getPersistentClass() 
+                                                                     : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) { 
+            return false;
+        }
+
+        Author author = (Author) object;
+
+        return this.getId() != null && this.getId() == author.getId();
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                                                                       .getPersistentClass().hashCode()
+                                              : this.getClass().hashCode();
+    }
 
 }
--- jpa-task/src/main/java/by/bsuir/jpatask/entity/News.java
+++ jpa-task/src/main/java/by/bsuir/jpatask/entity/News.java
@@ -1,12 +1,14 @@
 package by.bsuir.jpatask.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
+
+import org.hibernate.proxy.HibernateProxy;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.JoinColumn;
@@ -15,8 +17,8 @@ import jakarta.persistence.Table;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-@Data
+@Getter
+@Setter
 @SuperBuilder
 @jakarta.persistence.Entity
 @Table(name = "tbl_news")
@@ -37,5 +39,37 @@ public class News extends Entity {
     
     @Column(nullable = false)
     private LocalDateTime timeModified;
+    
+    @Override
+    public final boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null) {
+            return false;
+        }
+
+        Class<?> oEffectiveClass = object instanceof HibernateProxy ? ((HibernateProxy) object).getHibernateLazyInitializer()
+                                                                                               .getPersistentClass() 
+                                                                    : object.getClass();
+                                                                    
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                                                                                              .getPersistentClass() 
+                                                                     : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) { 
+            return false;
+        }
+
+        News news = (News) object;
+
+        return this.getId() != null && this.getId() == news.getId();
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                                                                       .getPersistentClass().hashCode()
+                                              : this.getClass().hashCode();
+    }
 
 }
