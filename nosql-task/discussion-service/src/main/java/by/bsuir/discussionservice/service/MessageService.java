0085f28068458a2005b11a621a72389c0d0e9e07
Dzmitry Haron <pitbull9846@gmail.com>
2024-10-09 23:43:21 +0300

    Create discussion service

---
+++ nosql-task/discussion-service/src/main/java/by/bsuir/discussionservice/service/MessageService.java
@@ -0,0 +1,63 @@
+package by.bsuir.discussionservice.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import by.bsuir.discussionservice.dto.request.MessageRequestTo;
+import by.bsuir.discussionservice.dto.response.MessageResponseTo;
+import by.bsuir.discussionservice.exception.EntityNotFoundException;
+import by.bsuir.discussionservice.exception.EntityNotSavedException;
+import by.bsuir.discussionservice.mapper.MessageMapper;
+import by.bsuir.discussionservice.repository.MessageRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MessageService {
+
+    private final MessageMapper MESSAGE_MAPPER;
+    private final MessageRepository MESSAGE_REPOSITORY;
+
+    public List<MessageResponseTo> getAll(Pageable restriction) {
+        return MESSAGE_REPOSITORY.findAll(restriction)
+                                 .stream()
+                                 .map(MESSAGE_MAPPER::toResponseTo)
+                                 .toList();
+    }
+
+    public MessageResponseTo getById(Long id) {
+        return MESSAGE_REPOSITORY.findByKey_Id(id)
+                                 .map(MESSAGE_MAPPER::toResponseTo)
+                                 .orElseThrow(() -> 
+                                     new EntityNotFoundException("Message", id));    
+    }
+
+    public MessageResponseTo save(MessageRequestTo message) {
+        return Optional.of(message)
+                       .map(MESSAGE_MAPPER::toEntity)
+                       .map(MESSAGE_REPOSITORY::save)
+                       .map(MESSAGE_MAPPER::toResponseTo)
+                       .orElseThrow(() -> 
+                           new EntityNotSavedException("Message", message.id()));
+    }
+
+    public MessageResponseTo update(MessageRequestTo message) {
+        return MESSAGE_REPOSITORY.findByKey_Id(message.id())
+                                 .map(entity -> MESSAGE_MAPPER.updateEntity(entity, message))
+                                 .map(MESSAGE_REPOSITORY::save)
+                                 .map(MESSAGE_MAPPER::toResponseTo)
+                                 .orElseThrow(() -> 
+                                     new EntityNotFoundException("Message", message.id()));
+    }
+
+    public void delete(Long id) {
+        MESSAGE_REPOSITORY.findByKey_Id(id)
+                          .ifPresentOrElse(MESSAGE_REPOSITORY::delete,
+                                           () -> { 
+                                               throw new EntityNotFoundException("Message", id); 
+                                           });  
+    }
+}
---
+++ nosql-task/discussion-service/src/test/java/by/bsuir/discussionservice/controller/MessageControllerTest.java
@@ -0,0 +1,34 @@
+package by.bsuir.discussionservice.controller;
+
+import java.util.UUID;
+
+import by.bsuir.discussionservice.dto.request.MessageRequestTo;
+import by.bsuir.discussionservice.dto.response.MessageResponseTo;
+
+public class MessageControllerTest extends RestControllerTest<MessageRequestTo, MessageResponseTo> {
+    private static final Long FK_NEWS_ID = 123L;
+
+    @Override
+    protected MessageRequestTo getRequestTo() {
+        return new MessageRequestTo(createId(), FK_NEWS_ID,
+                                    "Test content" + RANDOM_NUMBER_GENERATOR.nextInt(),
+                                    "Test country");
+    }
+
+    @Override
+    protected MessageRequestTo getUpdateRequestTo(MessageRequestTo originalRequest, Long updateEntityId) {
+        return new MessageRequestTo(updateEntityId,
+                                    originalRequest.newsId(),
+                                    "Test content" + RANDOM_NUMBER_GENERATOR.nextInt(),
+                                    "Test country");
+    }
+
+    @Override
+    protected String getRequestsMappingPath() {
+        return "/messages";
+    }
+
+    private static Long createId() {
+        return UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+    }
+}
